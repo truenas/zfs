4b4d34d943365eebc8369af9f398ae289dfeb463
Andrew Walker <awalker@ixsystems.com>
2020-11-09 10:06:30 -0500

    Implement NFSv41 ACLs through xattr
    This implements NFSv41 (RFC 5661) ACLs in a manner
    compatible with vfs_nfs4acl_xattr in Samba and
    nfs4xdr-acl-tools.
    
    There are three key areas of change in this commit:
    1) NFSv4 ACL management through system.nfs4_acl_xdr xattr.
      Install an xattr handler for "system.nfs4_acl_xdr" that
      presents an xattr containing full NFSv41 ACL structures
      generated through rpcgen using specification from the Samba
      project. This xattr is used by userspace programs to read and
      set permissions.
    
    2) add an i_op->permissions endpoint: zpl_permissions(). This
      is used by the VFS in Linux to determine whether to allow /
      deny an operation. Wherever possible, we try to avoid having
      to call zfs_access(). If kernel has NFSv4 patch for VFS, then
      perform more complete check of avaiable access mask.
    
    3) add capability-based overrides to secpolicy_vnode_access2()
      there are various situations in which ACL may need to be
      overridden based on capabilities. This logic is almost directly
      copied from Linux VFS. For instance, root needs to be able to
      always read / write ACLs (otherwise admin can get locked out
      from files).
    
    This is commit was initially inspired by work from Paul B. Henson
    to implement NFSv4.0 (RFC3530) ACLs in ZFS on Linux. Key areas of
    divergence are as follows:
    - ACL specification, xattr format, xattr name
    - Addition of handling for NFSv4 masks from Linux VFS
    - Addition of ACL overrides based on capabilities
    
    Signed-off-by: Andrew Walker <awalker@ixsystems.com>

--- module/os/linux/zfs/zpl_inode.c
+++ module/os/linux/zfs/zpl_inode.c
@@ -720,6 +720,7 @@ const struct inode_operations zpl_inode_operations = {
 #endif /* HAVE_SET_ACL */
 	.get_acl	= zpl_get_acl,
 #endif /* CONFIG_FS_POSIX_ACL */
+	.permission	= zpl_permission,
 };
 
 const struct inode_operations zpl_dir_inode_operations = {
@@ -753,6 +754,7 @@ const struct inode_operations zpl_dir_inode_operations = {
 #endif /* HAVE_SET_ACL */
 	.get_acl	= zpl_get_acl,
 #endif /* CONFIG_FS_POSIX_ACL */
+	.permission	= zpl_permission,
 };
 
 const struct inode_operations zpl_symlink_inode_operations = {
@@ -792,6 +794,7 @@ const struct inode_operations zpl_special_inode_operations = {
 #endif /* HAVE_SET_ACL */
 	.get_acl	= zpl_get_acl,
 #endif /* CONFIG_FS_POSIX_ACL */
+	.permission	= zpl_permission,
 };
 
 dentry_operations_t zpl_dentry_operations = {
