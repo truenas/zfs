406d9b12dde590f2334411c7ab23ab6e7750988a
Umer Saleem <usaleem@ixsystems.com>
2024-04-18 11:41:32 +0500

    JSON support zfs mount
    Signed-off-by: Umer Saleem <usaleem@ixsystems.com>

--- cmd/zfs/zfs_main.c
+++ cmd/zfs/zfs_main.c
@@ -310,7 +310,7 @@ get_usage(zfs_help_t idx)
 		    "[-s property]...\n\t    [-S property]... [-t type[,...]] "
 		    "[filesystem|volume|snapshot] ...\n"));
 	case HELP_MOUNT:
-		return (gettext("\tmount\n"
+		return (gettext("\tmount [-j]\n"
 		    "\tmount [-flvO] [-o opts] <-a|-R filesystem|"
 		    "filesystem>\n"));
 	case HELP_PROMOTE:
@@ -7387,12 +7387,15 @@ share_mount(int op, int argc, char **argv)
 	int do_all = 0;
 	int recursive = 0;
 	boolean_t verbose = B_FALSE;
+	boolean_t json = B_FALSE;
 	int c, ret = 0;
 	char *options = NULL;
 	int flags = 0;
+	nvlist_t *jsobj, *data, *item;
+	jsobj = data = item = NULL;
 
 	/* check options */
-	while ((c = getopt(argc, argv, op == OP_MOUNT ? ":aRlvo:Of" : "al"))
+	while ((c = getopt(argc, argv, op == OP_MOUNT ? ":ajRlvo:Of" : "al"))
 	    != -1) {
 		switch (c) {
 		case 'a':
@@ -7406,6 +7409,11 @@ share_mount(int op, int argc, char **argv)
 			break;
 		case 'l':
 			flags |= MS_CRYPT;
+			break;
+		case 'j':
+			json = B_TRUE;
+			jsobj = zfs_json_schema(0, 1);
+			data = fnvlist_alloc();
 			break;
 		case 'o':
 			if (*optarg == '\0') {
@@ -7440,6 +7448,11 @@ share_mount(int op, int argc, char **argv)
 
 	argc -= optind;
 	argv += optind;
+
+	if (json && argc != 0) {
+		(void) fprintf(stderr, gettext("too many arguments\n"));
+		usage(B_FALSE);
+	}
 
 	/* check number of arguments */
 	if (do_all || recursive) {
@@ -7544,12 +7557,27 @@ share_mount(int op, int argc, char **argv)
 			if (strcmp(entry.mnt_fstype, MNTTYPE_ZFS) != 0 ||
 			    strchr(entry.mnt_special, '@') != NULL)
 				continue;
-
-			(void) printf("%-30s  %s\n", entry.mnt_special,
-			    entry.mnt_mountp);
+			if (json) {
+				item = fnvlist_alloc();
+				fnvlist_add_string(item, "filesystem",
+				    entry.mnt_special);
+				fnvlist_add_string(item, "mountpoint",
+				    entry.mnt_mountp);
+				fnvlist_add_nvlist(data, entry.mnt_special,
+				    item);
+				fnvlist_free(item);
+			} else {
+				(void) printf("%-30s  %s\n", entry.mnt_special,
+				    entry.mnt_mountp);
+			}
 		}
 
 		(void) fclose(mnttab);
+		if (json) {
+			fnvlist_add_nvlist(jsobj, "data", data);
+			zcmd_print_json(jsobj);
+			fnvlist_free(data);
+		}
 	} else {
 		zfs_handle_t *zhp;
 
