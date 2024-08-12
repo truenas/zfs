a3defd0424743fad80f3a5202b4eed8ca894f4ef
Andrew Walker <awalker@ixsystems.com>
2024-08-12 12:41:55 -0700

    Fix namespace handling in zpl_permission (#244)
    This commit fixes user / idmap namespaces in zpl_permission.
    ZFS updates to address kernel changes were subtly broken and
    passing the wrong namespace to generic_permission().
    
    Since zpl_permission was initially written, zfs_zaccess() has
    become idmap-aware. This commit switches from using zfs_access to
    zfs_zaccess() and improves zfs_zaccess_aces_check() so that
    uids / gids in ACL entries are converted via idmap configuration
    prior to checking access.
    
    Signed-off-by: Andrew Walker <awalker@ixsystems.com>

--- module/os/linux/zfs/zpl_xattr.c
+++ module/os/linux/zfs/zpl_xattr.c
@@ -1435,6 +1435,7 @@ zpl_permission(struct inode *ip, int mask)
 {
 	int to_check = 0, i, ret;
 	cred_t *cr = NULL;
+	zfsvfs_t *zfsvfs = NULL;
 
 	/*
 	 * If NFSv4 ACLs are not being used, go back to
@@ -1444,9 +1445,10 @@ zpl_permission(struct inode *ip, int mask)
 	 */
 	if ((ITOZSB(ip)->z_acl_type != ZFS_ACLTYPE_NFSV4) ||
 	    ((ITOZ(ip)->z_pflags & ZFS_ACL_TRIVIAL && GENERIC_MASK(mask)))) {
-#if (defined(HAVE_IOPS_PERMISSION_USERNS) || \
-	defined(HAVE_IOPS_PERMISSION_IDMAP))
-		return (generic_permission(zfs_init_idmap, ip, mask));
+#if defined(HAVE_IOPS_PERMISSION_USERNS)
+		return (generic_permission(userns, ip, mask));
+#elif defined(HAVE_IOPS_PERMISSION_IDMAP)
+		return (generic_permission(idmap, ip, mask));
 #else
 		return (generic_permission(ip, mask));
 #endif
@@ -1463,9 +1465,10 @@ zpl_permission(struct inode *ip, int mask)
 	 * NFSv4 ACE. Pass back to default kernel permissions check.
 	 */
 	if (to_check == 0) {
-#if (defined(HAVE_IOPS_PERMISSION_USERNS) || \
-	defined(HAVE_IOPS_PERMISSION_IDMAP))
-		return (generic_permission(zfs_init_idmap, ip, mask));
+#if defined(HAVE_IOPS_PERMISSION_USERNS)
+		return (generic_permission(userns, ip, mask));
+#elif defined(HAVE_IOPS_PERMISSION_IDMAP)
+		return (generic_permission(idmap, ip, mask));
 #else
 		return (generic_permission(ip, mask));
 #endif
@@ -1542,7 +1545,24 @@ zpl_permission(struct inode *ip, int mask)
 		return (-ECHILD);
 	}
 
-	ret = -zfs_access(ITOZ(ip), to_check, V_ACE_MASK, cr);
+	zfsvfs = ZTOZSB(ITOZ(ip));
+
+	if ((ret = -zfs_enter_verify_zp(zfsvfs, ITOZ(ip), FTAG)) != 0)
+		return (ret);
+
+#if defined(HAVE_IOPS_PERMISSION_USERNS)
+	ret = -zfs_zaccess(ITOZ(ip), to_check, V_ACE_MASK, B_FALSE, cr,
+	    userns);
+#elif defined(HAVE_IOPS_PERMISSION_IDMAP)
+	ret = -zfs_zaccess(ITOZ(ip), to_check, V_ACE_MASK, B_FALSE, cr,
+	    idmap);
+#else
+	ret = -zfs_zaccess(ITOZ(ip), to_check, V_ACE_MASK, B_FALSE, cr,
+	    zfs_init_idmap);
+#endif
+
+	zfs_exit(zfsvfs, FTAG);
+
 	crfree(cr);
 	return (ret);
 }
