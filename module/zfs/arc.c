5f918d8b1fa6a40efe0a174280c800733b90fbae
Alexander Motin <mav@FreeBSD.org>
2025-05-14 10:34:14 -0400

    ARC: Notify dbuf cache about target size reduction
    ARC target size might drop significantly under memory pressure,
    especially if current ARC size was much smaller than the target.
    Since dbuf cache size is a fraction of the target ARC size, it
    might need eviction too.  Aside of memory from the dbuf eviction
    itself, it might help ARC by making more buffers evictable.
    
    Reviewed-by: Tony Hutter <hutter2@llnl.gov>
    Reviewed-by: Ameer Hamza <ahamza@ixsystems.com>
    Signed-off-by:	Alexander Motin <mav@FreeBSD.org>
    Sponsored by:	iXsystems, Inc.
    Closes #17314

--- module/zfs/arc.c
+++ module/zfs/arc.c
@@ -296,6 +296,7 @@
 #include <sys/dsl_pool.h>
 #include <sys/multilist.h>
 #include <sys/abd.h>
+#include <sys/dbuf.h>
 #include <sys/zil.h>
 #include <sys/fm/fs/zfs.h>
 #include <sys/callb.h>
@@ -4438,6 +4439,13 @@ arc_reduce_target_size(uint64_t to_free)
 	} else {
 		to_free = 0;
 	}
+
+	/*
+	 * Since dbuf cache size is a fraction of target ARC size, we should
+	 * notify dbuf about the reduction, which might be significant,
+	 * especially if current ARC size was much smaller than the target.
+	 */
+	dbuf_cache_reduce_target_size();
 
 	/*
 	 * Whether or not we reduced the target size, request eviction if the
