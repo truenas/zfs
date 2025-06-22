112b65d15e7ae37c142e79656ed9e95bad7fe467
Ameer Hamza <ahamza@ixsystems.com>
2025-06-23 01:13:25 +0500

    Mountpoint fixes
    Signed-off-by: Ameer Hamza <ahamza@ixsystems.com>

--- lib/libspl/include/os/linux/sys/stat.h
+++ lib/libspl/include/os/linux/sys/stat.h
@@ -32,6 +32,15 @@
 #include <sys/mount.h> /* for BLKGETSIZE64 */
 
 /*
+ * statx introduced in GLIBC 2.28
+ */
+#if defined(__GLIBC__) && __GLIBC_PREREQ(2, 28)
+#include <fcntl.h>
+#include <linux/stat.h>
+#define	HAVE_STATX 1
+#endif
+
+/*
  * Emulate Solaris' behavior of returning the block device size in fstat64().
  */
 static inline int
